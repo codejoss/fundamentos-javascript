d19edfa4b2547de89474f680468c005b78698425
Josue Quevedo <quevedoportas@icloud.com>
2025-06-09 20:08:42 +0000

    Se agrega clase de spread operators

--- 28-spread-operator.js
+++ 28-spread-operator.js
@@ -1,1 +1,26 @@
-// Clonar array
+// Copy an array
+const originalArray = [1,2 , 3, 4, 5];
+const copyArray = [...originalArray];
+
+console.log(`Original: ${originalArray}`);
+console.log(`Copy: ${copyArray}`);
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+const combineArray = [...array1, ...array2];
+console.log(`Array1: ${array1}`);
+console.log(`Array2: ${array2}`);
+console.log(`Combinado: ${combineArray}`);
+
+const newArrayCOmbine = [...array2, 'amarillo'];
+console.log(newArrayCOmbine);
+
+// Pass elements to a function
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+const numbers = [1, 2, 3];
+console.log(sum(...numbers));
